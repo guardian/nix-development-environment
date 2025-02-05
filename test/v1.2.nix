913b2cc23133a5445540b228033361d7e6d2d08c
Emily Bourke <emily.bourke@guardian.co.uk>
2025-02-05 14:17:35 +0000

    Add test of v1.2 usage
    I’m about to add a v1.2 tag now that I’ve added the extraInputs
    attribute, so I’m adding a second test of the newer usage pattern.

---
+++ test/v1.2.nix
@@ -0,0 +1,20 @@
+let
+  pkgs = import <nixpkgs> { };
+  guardianNix = builtins.fetchGit {
+    url = "git@github.com:guardian/guardian-nix.git";
+    ref = "refs/tags/v1";
+  };
+  guardianDev = import "${guardianNix.outPath}/guardian-dev.nix" pkgs;
+
+  firstCommand = pkgs.writeShellApplication {
+    name = "first-command";
+    runtimeInputs = [ ];
+    text = ''
+      echo "first-command"
+    '';
+  };
+in guardianDev.devEnv {
+  name = "test-v1.2";
+  commands = [ firstCommand ];
+  extraInputs = [ pkgs.hello ];
+}
