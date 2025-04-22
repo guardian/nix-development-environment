47b097bd40d16543d3763d366a5f8a83d41a1708
Emily Bourke <emily.bourke@guardian.co.uk>
2025-04-22 18:32:12 +0100

    WIP: support named sets of commands
    The motivating example for this is composer, where it’d be nice to offer
    the user the choice between running regular composer or running the
    staging app (which are mutually exclusive because they run on the same
    port) with a nice command interface.

--- guardian-dev.nix
+++ guardian-dev.nix
@@ -1,48 +1,52 @@
 { pkgs, ... }:
+let
+  commandSet = { appName, commandSetName, commands }:
+    let
+      allCommandsJson = pkgs.writeTextFile {
+        name = "${commandSetName}-all-commands.json";
+        text = builtins.toJSON commands;
+      };
+      listCommandsJson = pkgs.writeShellApplication {
+        name = "${commandSetName}-list-commands-json";
+        runtimeInputs = [ pkgs.jq ];
+        text = ''
+          jq . "${allCommandsJson}"
+        '';
+      };
+      listCommands = pkgs.writeShellApplication {
+        name = "${commandSetName}-list-commands";
+        runtimeInputs = [ pkgs.jq ];
+        text = "list-commands-json | jq -r 'map([.] | @tsv) | .[]'";
+      };
 
-{
+      runAllTmux = pkgs.writeShellApplication {
+        name = "${commandSetName}-run-all-tmux";
+        runtimeInputs = [ pkgs.tmux ];
+        text = builtins.foldl' (acc: command:
+          if acc == "" then
+            builtins.concatStringsSep " \";\" " [
+              ''tmux new-session "${command}/bin/${command.name}"''
+              "set-option remain-on-exit on"
+              ''rename-session "${appName}:${commandSetName}"''
+            ]
+          else
+            acc
+            + " \";\" new-window -n ${command.name} \"${command}/bin/${command.name}\"")
+          "" commands;
+      };
+
+      runAllBackground = pkgs.writeShellApplication {
+        name = "${commandSetName}-run-all-background";
+        runtimeInputs = [ ];
+        text = builtins.concatStringsSep " &\n"
+          (builtins.map (command: "${command}/bin/${command.name}") commands);
+      };
+    in { };
+in {
   devEnv = { name, commands, extraInputs ? [ ], ... }:
     pkgs.mkShellNoCC {
       nativeBuildInputs = let
-        allCommandsJson = pkgs.writeTextFile {
-          name = "all-commands.json";
-          text = builtins.toJSON commands;
-        };
-        listCommandsJson = pkgs.writeShellApplication {
-          name = "list-commands-json";
-          runtimeInputs = [ pkgs.jq ];
-          text = ''
-            jq . "${allCommandsJson}"
-          '';
-        };
-        listCommands = pkgs.writeShellApplication {
-          name = "list-commands";
-          runtimeInputs = [ pkgs.jq ];
-          text = "list-commands-json | jq -r 'map([.] | @tsv) | .[]'";
-        };
 
-        runAllTmux = pkgs.writeShellApplication {
-          name = "run-all-tmux";
-          runtimeInputs = [ pkgs.tmux ];
-          text = builtins.foldl' (acc: command:
-            if acc == "" then
-              builtins.concatStringsSep " \";\" " [
-                ''tmux new-session "${command}/bin/${command.name}"''
-                "set-option remain-on-exit on"
-                ''rename-session "${name}"''
-              ]
-            else
-              acc
-              + " \";\" new-window -n ${command.name} \"${command}/bin/${command.name}\"")
-            "" commands;
-        };
-
-        runAllBackground = pkgs.writeShellApplication {
-          name = "run-all-background";
-          runtimeInputs = [ ];
-          text = builtins.concatStringsSep " &\n"
-            (builtins.map (command: "${command}/bin/${command.name}") commands);
-        };
       in commands ++ extraInputs ++ [
         pkgs.tmux
         pkgs.jq
